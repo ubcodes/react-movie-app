e01bfdfa9049f7eb79e461b391f99ba276f5a640
UB30-4 <101749091+UB30-4@users.noreply.github.com>
2023-09-26 23:46:05 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import SearchIcon from './search.svg';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
+//`useState` is a function in React (a JavaScript library for building user interfaces) that helps you manage and change data within your components. It allows you to create and keep track of variables that can hold information, and when you update these variables, React automatically re-renders your component to reflect the changes on the screen. It's like having a memory within your component to store and modify data as needed.
+
+//`useEffect` is a function in React (a JavaScript library for building user interfaces) that allows you to perform side effects in your components. Side effects are actions that happen in your component, such as data fetching, DOM manipulation, or setting up subscriptions. You use `useEffect` to tell React to perform these actions after the component has rendered or when specific values change. It helps you manage tasks that need to happen in response to changes in your component's state or props.
+
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
